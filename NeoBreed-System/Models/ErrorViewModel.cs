1cbf706cc08a5c7cf660d7d407ac7aec30952942
pc <pc@DESKTOP-1NGSRAS>
2025-02-22 14:48:31 +0200

    Add project files.

---
+++ NeoBreed-System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NeoBreed_System.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
